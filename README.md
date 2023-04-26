74f5f63a52f6afa18c067e488e6b1a78f30055d0
AlvinKuria <alvinkuria18@gmail.com>
2023-04-26 10:34:11 +0100

    Made Layout of Webpage

---
+++ README.md
@@ -0,0 +1,3 @@
+ I am building a browser version of something between a sketchpad and an Etch-A-Sketch.
+
+ This is to show and strengthen my DOM manipulation skills.
---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body')
+const h1 = document.createElement('h1');
+const header = document.querySelector('.header');
+const container = document.querySelector('.container');
+const button = document.querySelector('.button');
+
+container.classList.add('container');

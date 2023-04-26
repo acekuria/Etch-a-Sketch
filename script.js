98587744eebf1134937550847108e44e338e1433
AlvinKuria <alvinkuria18@gmail.com>
2023-04-26 14:22:45 +0100

    grid and button working

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 const body = document.querySelector('body')
 const h1 = document.createElement('h1');
 const header = document.querySelector('.header');
-const container = document.querySelector('.container');
+const board = document.querySelector('.board');
 const button = document.querySelector('.button');
 
-container.classList.add('container');
+
+function populateBoard(size) {
+  board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  for (let i = 0; i < size**2; i++) {
+    let square = document.createElement('div');
+    square.style.backgroundColor = 'blue';
+    board.insertAdjacentElement('beforeend',square)
+  }
+
+}
+
+populateBoard(20);
+
+function changeSize(input) {
+  if (input <= 100 && input >= 2) {
+    populateBoard(input);
+  } else {
+    console.log('ERROR')
+  }
+}
+
+
+
